b02d3a582a1fe925d7e5a091580297a6a9abcb26
Vani <65925588+saiykUwU@users.noreply.github.com>
2020-05-28 11:30:17 +1200

    Import files from project
    Include Library files & reference

---
+++ oos!tool/oos_Tool.py
@@ -0,0 +1,7 @@
+import tkinter
+window = tkinter.Tk()
+window.title('Bruh')
+label = tkinter.Label(window, text = "LabelText", font=("Arial", 25)).pack()
+window.geometry('600x200')
+
+window.mainloop()
